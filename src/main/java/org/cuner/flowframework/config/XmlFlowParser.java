1e6aedfc21561628c85874330aa1ed431d95645c
Cuner <469464794@qq.com>
2018-07-18 18:34:05 +0800

    init project construction

---
+++ src/main/java/org/cuner/flowframework/config/XmlFlowParser.java
@@ -0,0 +1,7 @@
+package org.cuner.flowframework.config;
+
+/**
+ * Created by houan on 18/7/16.
+ */
+public class XmlFlowParser {
+}
---
+++ src/main/java/org/cuner/flowframework/core/Flow.java
@@ -0,0 +1,7 @@
+package org.cuner.flowframework.core;
+
+/**
+ * Created by houan on 18/7/18.
+ */
+public interface Flow {
+}
